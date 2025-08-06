c922dded60aa4b34cc2c81583541ac6b770f4ecb
khadaroo <khadaro2024@gmail.com>
2025-08-06 22:48:05 +0300

    create a view context and provided

---
+++ src/contexts/ViewContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const ViewContext = createContext;
+
+export function ViewProvider({ children }) {
+  const [view, setView] = useState("grid");
+
+  return (
+    <ViewContext.Provider value={{ view, setView }}>
+      {children}
+    </ViewContext.Provider>
+  );
+}
